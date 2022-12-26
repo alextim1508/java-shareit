42a41bb55110d09ba7d858d6b6641f6a877a9dbe
Alex <atimofeev1508@gmail.com>
2022-12-26 15:53:49 +0300

    code style

--- src/test/java/ru/practicum/shareit/item/dto/FullItemDtoOutTest.java
+++ src/test/java/ru/practicum/shareit/item/dto/FullItemDtoOutTest.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.item.dto;
 
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
